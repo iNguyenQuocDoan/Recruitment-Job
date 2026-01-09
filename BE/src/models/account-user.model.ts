245c9e1cb1ad35bf6f58334681ab328a70684801
iNguyenQuocDoan <quocdoanfb@gmail.com>
2026-01-09 20:21:59 +0700

    Model account user

---
+++ BE/src/models/account-user.model.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    fullName: String,
+    email: String,
+    password: String,
+  },
+  { timestamps: true }
+);
+
+const AccountUser = mongoose.model("AccountUser", schema, "account_users");
+
+export default AccountUser;
